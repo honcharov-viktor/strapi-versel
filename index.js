844069fa7fe38e7ad32ece8962b61e9359fd5900
Viktor Honcharov <honcharov.viktor.v@gmail.com>
2023-11-15 05:44:13 +0200

    feat(app): disable serveAdminPanel

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 // index.js
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+
 const PORT = process.env.PORT || 3000;
+const PORT_CMS = process.env.PORT_CMS || 1337;
+const CMS_SERVICE_URL = `http://localhost:${PORT_CMS}`;
+const FRONT_SERVICE_URL = "http://localhost:3000";
 
+const allowedCMSPrifix = ['admin', 'i18n', 'content-manager'];
+const allowedCMSRegexp = new RegExp(`/(${allowedCMSPrifix.join('|')})`);
 
-app.get('/home', (req, res) => {
-  res.status(200).json('Welcome, your app is working well');
-})
+app.use(allowedCMSRegexp, createProxyMiddleware({
+    target: CMS_SERVICE_URL,
+    changeOrigin: true
+}));
 
+app.use('/wp-admin', express.static('build'));
 
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+    console.log(`Server running at http://localhost:${PORT}`);
 });
 
 // Export the Express API
