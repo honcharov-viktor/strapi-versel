9dadda573d8ddab1ea5969b3ae4f629f1ffbf041
Viktor Honcharov <honcharov.viktor.v@gmail.com>
2023-11-15 09:52:01 +0200

    fix(app): fix typo

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const Strapi = require("@strapi/strapi/dist/Strapi");
+const strapi = require("@strapi/strapi");
 
 const serverless = require("serverless-http");
 
 const appDir = process.cwd();
 
 module.exports.handler = async (event, context) => {
-  const strapiInstance = Strapi({ appDir, distDir: appDir });
+  const strapiInstance = strapi.Strapi({ appDir, distDir: appDir });
   await strapiInstance.start();
   const handler = serverless(strapiInstance.app);
   return handler(event, context);
