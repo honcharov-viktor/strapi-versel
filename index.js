fac712c767ee2838dc7027a91c8df8f5f5f843f1
Viktor Honcharov <honcharov.viktor.v@gmail.com>
2023-11-15 10:07:45 +0200

    fix(app)

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 const strapi = require("@strapi/strapi");
 
-const serverless = require("serverless-http");
+// const serverless = require("serverless-http");
 
 const appDir = process.cwd();
 
-module.exports.handler = async (event, context) => {
-  const strapiInstance = strapi.Strapi({ appDir, distDir: appDir });
-  await strapiInstance.start();
-  const handler = serverless(strapiInstance.app);
-  return handler(event, context);
-};
+const strapiInstance = strapi.Strapi({ appDir, distDir: appDir });
+strapiInstance.start();
+
+module.exports = strapiInstance.server;
+
+// module.exports.handler = async (event, context) => {
+//   const strapiInstance = strapi.Strapi({ appDir, distDir: appDir });
+//   await strapiInstance.start();
+//   const handler = serverless(strapiInstance.app);
+//   return handler(event, context);
+// };
+
+// // index.js
+// const express = require('express')
+// const app = express()
+// const PORT = process.env.PORT || 3000;
+
+
+// app.get('/home', (req, res) => {
+//   res.status(200).json('Welcome, your app is working well');
+// })
+
+
+// app.listen(PORT, () => {
+//   console.log(`Server running at http://localhost:${PORT}`);
+// });
+
+// // Export the Express API
+// module.exports = app
